b30a8af76a8b35a0411d3d3588dfecab79d11a61
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2025-10-21 00:05:55 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -8,17 +8,6 @@ enabled_site_setting :discoursecolor_enabled
 
 register_asset 'stylesheets/user-groups-injector.scss'
 
-add_admin_route "discoursecolor.title", "discoursecolor"
-
-module ::Discoursecolor
-  PLUGIN_NAME = "discoursecolor"
-
-  class Engine < ::Rails::Engine
-    engine_name PLUGIN_NAME
-    isolate_namespace Discoursecolor
-  end
-end
-
 after_initialize do
   # Add group information to user serializers
   add_to_serializer(:basic_user, :user_groups) do
@@ -63,24 +52,28 @@ after_initialize do
     group_colors[highest_group] || '#000000'
   end
 
-  # API endpoint to get all groups
-  class DiscoursecolorController < ::Admin::AdminController
-    requires_plugin Discoursecolor::PLUGIN_NAME
+  # Add admin route for plugin
+  add_admin_route "discoursecolor.title", "discoursecolor"
 
-    def groups
-      groups = Group.all.map do |group|
-        {
-          id: group.id,
-          name: group.name,
-          user_count: group.users.count
-        }
+  # Simple API endpoint using standard Discourse routing
+  module ::Discoursecolor
+    class AdminController < ::Admin::AdminController
+      def groups
+        groups = Group.all.map do |group|
+          {
+            id: group.id,
+            name: group.name,
+            user_count: group.users.count
+          }
+        end
+
+        render json: { groups: groups }
       end
-
-      render json: { groups: groups }
     end
   end
 
-  Discoursecolor::Engine.routes.draw do
-    get "/groups" => "discoursecolor#groups"
+  # Add route without using an engine
+  Discourse::Application.routes.append do
+    get "/admin/plugins/discoursecolor/groups" => "discoursecolor/admin#groups"
   end
 end
