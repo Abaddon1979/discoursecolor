f921584c3f92cbe432258565050f2da6f7c0ce12
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2025-10-21 12:07:17 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -11,45 +11,71 @@ register_asset 'stylesheets/user-groups-injector.scss'
 after_initialize do
   # Add group information to user serializers
   add_to_serializer(:basic_user, :user_groups) do
-    object.groups.pluck(:name)
+    object.groups.pluck(:name) rescue []
   end
 
   add_to_serializer(:basic_user, :highest_ranked_group) do
-    group_names = object.groups.pluck(:name)
-    rankings = SiteSetting.discoursecolor_group_rankings.split(',')
-    
-    # Normalize group names (plural to singular)
-    normalized_groups = group_names.map { |name| name.singularize }
-    
-    # Find the highest ranked group
-    highest_group = rankings.find { |rank| normalized_groups.include?(rank.singularize) }
-    highest_group || group_names.first
+    begin
+      group_names = object.groups.pluck(:name)
+      return nil if group_names.blank?
+      
+      rankings = SiteSetting.discoursecolor_group_rankings.presence || "admin,staff,moderator,trust_level_4,trust_level_3,trust_level_2,trust_level_1"
+      rankings = rankings.split(',')
+      
+      # Normalize group names (plural to singular)
+      normalized_groups = group_names.map { |name| name.singularize }
+      
+      # Find the highest ranked group
+      highest_group = rankings.find { |rank| normalized_groups.include?(rank.singularize) }
+      highest_group || group_names.first
+    rescue => e
+      Rails.logger.warn "Discoursecolor: Error calculating highest_ranked_group: #{e.message}"
+      nil
+    end
   end
 
   add_to_serializer(:basic_user, :group_color) do
-    group_colors = JSON.parse(SiteSetting.discoursecolor_group_colors)
-    highest_group = self.highest_ranked_group
-    group_colors[highest_group] || '#000000'
+    begin
+      group_colors = JSON.parse(SiteSetting.discoursecolor_group_colors.presence || '{"admin":"#ff0000","staff":"#00ff00","moderator":"#0000ff","trust_level_4":"#ffa500","trust_level_3":"#800080","trust_level_2":"#008080","trust_level_1":"#808080"}')
+      highest_group = self.highest_ranked_group
+      group_colors[highest_group] || '#000000'
+    rescue => e
+      Rails.logger.warn "Discoursecolor: Error calculating group_color: #{e.message}"
+      '#000000'
+    end
   end
 
   # Add same methods to user serializer
   add_to_serializer(:user, :user_groups) do
-    object.groups.pluck(:name)
+    object.groups.pluck(:name) rescue []
   end
 
   add_to_serializer(:user, :highest_ranked_group) do
-    group_names = object.groups.pluck(:name)
-    rankings = SiteSetting.discoursecolor_group_rankings.split(',')
-    
-    normalized_groups = group_names.map { |name| name.singularize }
-    highest_group = rankings.find { |rank| normalized_groups.include?(rank.singularize) }
-    highest_group || group_names.first
+    begin
+      group_names = object.groups.pluck(:name)
+      return nil if group_names.blank?
+      
+      rankings = SiteSetting.discoursecolor_group_rankings.presence || "admin,staff,moderator,trust_level_4,trust_level_3,trust_level_2,trust_level_1"
+      rankings = rankings.split(',')
+      
+      normalized_groups = group_names.map { |name| name.singularize }
+      highest_group = rankings.find { |rank| normalized_groups.include?(rank.singularize) }
+      highest_group || group_names.first
+    rescue => e
+      Rails.logger.warn "Discoursecolor: Error calculating highest_ranked_group: #{e.message}"
+      nil
+    end
   end
 
   add_to_serializer(:user, :group_color) do
-    group_colors = JSON.parse(SiteSetting.discoursecolor_group_colors)
-    highest_group = self.highest_ranked_group
-    group_colors[highest_group] || '#000000'
+    begin
+      group_colors = JSON.parse(SiteSetting.discoursecolor_group_colors.presence || '{"admin":"#ff0000","staff":"#00ff00","moderator":"#0000ff","trust_level_4":"#ffa500","trust_level_3":"#800080","trust_level_2":"#008080","trust_level_1":"#808080"}')
+      highest_group = self.highest_ranked_group
+      group_colors[highest_group] || '#000000'
+    rescue => e
+      Rails.logger.warn "Discoursecolor: Error calculating group_color: #{e.message}"
+      '#000000'
+    end
   end
 
   # Add admin route for plugin
@@ -59,15 +85,20 @@ after_initialize do
   module ::Discoursecolor
     class AdminController < ::Admin::AdminController
       def groups
-        groups = Group.all.map do |group|
-          {
-            id: group.id,
-            name: group.name,
-            user_count: group.users.count
-          }
-        end
+        begin
+          groups = Group.all.map do |group|
+            {
+              id: group.id,
+              name: group.name,
+              user_count: group.users.count
+            }
+          end
 
-        render json: { groups: groups }
+          render json: { groups: groups }
+        rescue => e
+          Rails.logger.error "Discoursecolor: Error fetching groups: #{e.message}"
+          render json: { groups: [] }
+        end
       end
     end
   end
